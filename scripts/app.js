25d7e567293d83472805a847c9f3cf7786f71dac
raahinn <rahinn11@gmail.com>
2020-09-07 18:07:35 +0600

    Showing weather update in UI.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,43 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const UiUpdate = (data) => {
+    const cityData = data.cityData;
+    const weather = data.weather;
+
+    console.log(data.cityData);
+    console.log(data.weather);
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityData.LocalizedName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>temp</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    //  remove the d-none class
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+
+};
 
 const updateCity = async city => {
     //console.log(city);
     const cityData = await getCity(city);
     const weather = await getWeather(cityData.Key);
 
-    return {
-        cityData: cityData,
-        weather: weather
-    }
+    // return {
+    //     cityData: cityData,
+    //     weather: weather
+    // }
+
+    // Object Shorthand Notation
+    // object key and value name same
+    return { cityData, weather };
 
 };
 
@@ -19,11 +48,12 @@ cityForm.addEventListener('submit', e => {
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    // update the ui
-    updateCity(city).then(data => {
-        console.log(data);
-    }).catch(err => {
-        console.log(err);
-    });
+
+    updateCity(city)
+        .then(data => {
+            UiUpdate(data);
+        }).catch(err => {
+            console.log(err);
+        });
 
 });
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,10 +1,10 @@
 // api key from accu weather
-const key = '1lVmy5meSp8KAg4FdXCy5M601nGhOGc5';
+//const key = '1lVmy5meSp8KAg4FdXCy5M601nGhOGc5';
 
 // get weather infomation
 const getWeather = async (id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    //const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -18,7 +18,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    //const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
