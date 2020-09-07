498ddd9e942d3225ef5dfd2853b0e03f82bea198
raahinn <rahinn11@gmail.com>
2020-09-07 19:32:03 +0600

    Weather Icons & images

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,48 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
 
 const UiUpdate = (data) => {
-    const cityData = data.cityData;
-    const weather = data.weather;
 
-    console.log(data.cityData);
-    console.log(data.weather);
+    // const cityData = data.cityData;
+    // const weather = data.weather;
 
+    // destructure properties of object
+    const { cityData, weather } = data;
+    console.log(data);
+    // update details template
     details.innerHTML = `
     <h5 class="my-3">${cityData.LocalizedName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
     <div class="display-4 my-4">
-        <span>temp</span>
+        <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
     </div>
     `;
+
+    // update the night/day & icon image
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // let timeSrc = null;
+    // if (weather.IsDayTime) {
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // }
+
+    // Ternary Operator
+    let timeSrc = weather.IsDayTime ? timeSrc = 'img/day.svg' : timeSrc = 'img/night.svg';
+    if (weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
 
     //  remove the d-none class
     if (card.classList.contains('d-none')) {
