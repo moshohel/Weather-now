7a7bb4e7cbc8dfc4b4477309c42afa48cd7cc336
raahinn <rahinn11@gmail.com>
2020-09-07 14:19:29 +0600

    Api setup & data fetch.

---
+++ scripts/forecast.js
@@ -0,0 +1,38 @@
+// api key from accu weather
+const key = 'OdJlk8vaKGxy0ZywRh0PKwfDw73lqO7O';
+
+// get weather infomation
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    //console.log(data);
+    return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data[0]);
+
+    return data[0];
+};
+
+getCity('DHAKA').then(data => {
+    console.log(`Dhaka key : ${data.Key}`);
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(`Dhaka weather is today : ${data.WeatherText}`);
+}).catch(err => console.log(err));
+
